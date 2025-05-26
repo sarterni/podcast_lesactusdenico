206c02fa61ec9d8d5f9a674b0fa273b5f51082ef
SARTER Nicolas <nicolas.sarter4@etu.univ-lorraine.fr>
2025-05-26 15:18:43 +0200

    Initial commit

---
+++ OnVousEnParle.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/HomePage.css?v=<?php echo filemtime('css/HomePage.css'); ?>" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="logo">
+        <img src="./img/FruitDeLaPassionFM.png" alt="">
+        <h1>On Vous En Parle</h1>
+        <P>La radio qui va éplucher votre aubergine.</P>
+    </div>
+
+    <div class="search-bar">
+        <input type="text" placeholder="Rechercher un podcast">
+        <button>Rechercher</button>
+
+    </div>
+
+    <div class="emissions">
+
+        <img src="./img/FruitDeLaPassionFM.png" alt="">
+        <h2>Émission du 08 mars 2024</h2>
+        <p>Pourquoi il est impossible d'être totalement végétarien, L'influence de la cuisine sur le cinéma, ou encore
+            Comment apprendre à cuisiner avec la réalité virtuelle, sans oublier notre traditionnel débat : « Le
+            véganisme est-il profitable à tous ? ».</p>
+
+        <div class="infos">
+            <p class="date">9 mars 2024</p>
+            <a href="https://youtu.be/CN692uglgTU?si=56RNYw2Uok1clNI7" style="text-decoration:none;">
+                <button class="watch">Play on youtube</button>
+            </a>
+            <audio controls>
+                <source src="./Fruit de la passion FM.mp3" type="audio/mpeg">
+                Your browser does not support the audio element.
+            </audio>
+        </div>
+
+    </div>
+
+    
+
+
+    <div class="container">
+        <div class="a-propos">
+            <h2>A propos de la radio</h2>
+            <h3>Tous les jours en continu</h3>
+            <h4>Le meilleur des actualités culinaires</h4>
+            <p>Notre radio Fruit de la passion FM est une radio ayant pour thème principal la cuisine. C’est une radio
+                pas
+                très sérieuse mais qui vise quand même à instruire ses auditeurs au niveau culinaire. Nous avons
+                plusieurs
+                émissions dont l’actualité culinaire présentée par Rémi Choffat, les chroniques culinaires présentées
+                par
+                Kateryna Babachanakh et Nicolas Sarter, et les débats présentés par 2 personnes de bord fruitier
+                totalement
+                différents : Mathieu Graff et Rémi Choffat. Évidemment, Fruit de la passion FM est une radio qui tient à
+                ses
+                auditeurs et fait donc gagner des prix à ceux-ci à travers chaque émission.
+
+            </p>
+        </div>
+
+        <div class="equipe">
+            <h1 id="h1-equipe">l'équipe</h1>
+            <div id="no-1" class="membre">
+                <a href="https://webetu.iutnc.univ-lorraine.fr/www/choffat2u/" style="text-decoration:none;">
+                    <button class="follow-me"><span>🐀</span></button>
+                </a>
+                <p class="post">fondateur</p>
+
+                <p id="text">
+                    Rémi Choffat, Maître Choffat, Brigitte, Carlos
+
+                </p>
+            </div>
+
+            <div id="no-2" class="membre">
+                <a href="" style="text-decoration:none;">
+                    <button class="follow-me"><span>🐀</span></button>
+                </a>
+                <p class="post">monteur</p>
+
+                <p id="text">
+                    Mathieu Graff, Professeur Graff, Jean
+
+
+                </p>
+
+            </div>
+
+            <div id="no-3" class="membre">
+                <a href="https://weather-app.nicodigitalsstudio.com/" style="text-decoration:none;">
+                    <button class="follow-me"><span>🐀</span></button>
+                </a>
+                <p class="post">écriture</p>
+
+                <p id="text">
+                    Kateryna Babachanakh, Kateryna
+
+                </p>
+
+            </div>
+            <div id="no-4" class="membre">
+                <a href="https://weather-app.nicodigitalsstudio.com/" style="text-decoration:none;">
+                    <button class="follow-me"><span>🐀</span></button>
+                </a>
+                <p class="post">fondateur</p>
+
+                <p id="text">
+                    Rémi Choffat, Maître Choffat, Brigitte, Carlos
+
+                </p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
